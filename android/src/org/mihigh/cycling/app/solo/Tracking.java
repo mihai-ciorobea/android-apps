d29ca1f79044d837ce1ba8fe9aa58641fdbf1acf
Mihai C <mciorobe@adobe.com>
2014-08-13 14:32:34 +0300

    Added Tracking for stats

---
+++ android/src/org/mihigh/cycling/app/solo/Tracking.java
@@ -0,0 +1,42 @@
+package org.mihigh.cycling.app.solo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import android.location.Location;
+
+public class Tracking {
+
+    private List<Location> positions = new ArrayList<Location>();
+
+    public void addLocation(Location location) {
+        positions.add(location);
+    }
+
+    public List<Integer> get5MinActivity() {
+        List<Integer> activity = new ArrayList<Integer>();
+
+        Location lastLocation = null;
+        float distance = 0;
+
+        for (Location location : positions) {
+            if (lastLocation == null) {
+            }
+            distance += location.distanceTo(lastLocation);
+
+            if (location.getElapsedRealtimeNanos() - lastLocation.getElapsedRealtimeNanos() >= 5L * 60 * 1000 * 1000 * 1000) {
+                activity.add((int) distance);
+
+                distance = 0;
+                lastLocation = location;
+            }
+        }
+
+        activity.add((int) distance);
+        return activity;
+    }
+
+
+}
+
+
